00bac36a41f23b182721bcd38803c954b3b1566d
高辉 <hgao@xiaohongshu.com>
2020-05-11 08:24:40 +0800

    add recyclerview test

--- app/src/main/java/com/gaohui/android/code/collection/MainActivity.kt
+++ app/src/main/java/com/gaohui/android/code/collection/MainActivity.kt
@@ -2,6 +2,9 @@ package com.gaohui.android.code.collection
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.PagerSnapHelper
+import android.support.v7.widget.RecyclerView
 import com.gaohui.android.code.collection.view.CustomAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,21 +12,21 @@ class MainActivity : AppCompatActivity() {
 
     private val mDataList = ArrayList<Any>()
 
+    private val pagerSnapHelper = PagerSnapHelper()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        parentRecyclerView.refreshLayoutManager()
-
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        pagerSnapHelper.attachToRecyclerView(recyclerView)
         val customAdapter =  CustomAdapter(mDataList)
-        customAdapter.setFragmentManager(supportFragmentManager)
-        parentRecyclerView.adapter = customAdapter
-
+        recyclerView.adapter = customAdapter
         for (i in 0..8) {
             mDataList.add("text: $i")
         }
-        mDataList.add(1)
 
-        parentRecyclerView.adapter?.notifyDataSetChanged()
+        recyclerView?.adapter?.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/gaohui/android/code/collection/view/CustomAdapter.kt
+++ app/src/main/java/com/gaohui/android/code/collection/view/CustomAdapter.kt
@@ -1,16 +1,11 @@
 package com.gaohui.android.code.collection.view
 
-import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentStatePagerAdapter
-import android.support.v4.view.ViewPager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import com.gaohui.android.code.collection.BlankFragment
 import com.gaohui.android.code.collection.R
 
 
@@ -22,8 +17,6 @@ class CustomAdapter(
     private val TYPE_TEXT = 0
     private val TYPE_CATEGORY = 1
 
-    private var mCategoryView:SimpleCategoryViewHolder? = null
-
 
     override fun getItemViewType(position: Int): Int {
         if(dataSet[position] is String) {
@@ -34,82 +27,25 @@ class CustomAdapter(
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if(viewType == TYPE_TEXT) {
-            val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.layout_item_text, viewGroup, false)
-            return SimpleTextViewHolder(v)
-        } else {
-            val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.layout_category_text, viewGroup, false)
-            val holder = SimpleCategoryViewHolder(v)
-            mCategoryView = holder
-            return holder
-        }
-
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.layout_item_text, viewGroup, false)
+        Log.d("gaohui","onCreateViewHolder")
+        return SimpleTextViewHolder(v)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, pos: Int) {
         if(holder is SimpleTextViewHolder) {
+            Log.d("gaohui","onBindViewHolder")
             holder.mTv.text = (dataSet[pos] as String)
-        } else if(holder is SimpleCategoryViewHolder) {
-            holder.bindData()
         }
 
     }
 
     override fun getItemCount() = dataSet.size
 
-    fun getCategoryView():SimpleCategoryViewHolder? {
-        return mCategoryView
-    }
-
-    var fm:FragmentManager? = null
-
-    fun setFragmentManager(supportFragmentManager: FragmentManager?) {
-        fm = supportFragmentManager
-
-    }
-
     private inner class SimpleTextViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
          val mTv: TextView = itemView.findViewById(R.id.textView) as TextView
 
-    }
-
-    inner class SimpleCategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val fragmentList = ArrayList<Fragment>()
-        private val titleList = ArrayList<String>()
-
-         val mTabLayout: TabLayout = itemView.findViewById(R.id.tabs) as TabLayout
-         val mViewPager: ViewPager = itemView.findViewById(R.id.viewPager) as ViewPager
-
-        fun bindData() {
-            fragmentList.clear()
-            titleList.clear()
-
-            for (i in 0..5) {
-                titleList.add("tab $i")
-                fragmentList.add(BlankFragment.newInstance())
-            }
-
-            mViewPager.adapter = IndexPagerAdapter(fm!!,titleList,fragmentList)
-            mTabLayout.setupWithViewPager(mViewPager)
-        }
-
-        internal inner class IndexPagerAdapter(fm: FragmentManager, private val titleList: List<String>, val fragmentList: List<Fragment>) : FragmentStatePagerAdapter(fm) {
-
-            override fun getItem(position: Int): Fragment {
-                return fragmentList.get(position)
-            }
-
-            override fun getCount(): Int {
-                return titleList.size
-            }
-
-            override fun getPageTitle(position: Int): CharSequence? {
-                return titleList[position]
-            }
-
-        }
     }
 
 }
